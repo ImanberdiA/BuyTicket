bc06a979cf9f69b9f84d647bf33ee6650a7c7130
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2018-03-30 21:53:08 +0300

    q

--- search/searchController.js
+++ search/searchController.js
@@ -3,14 +3,19 @@ var searchServiceObject = new searchService.SearchService();
 
 class SearchController {
 
-
-
         async getListFlights(req, res) {
-            var result;
+            var result, j = 0;
 
             if(/^\d{4}-\d{1,2}-\d\d$/.test(req.headers.date)){
-                console.log(new Date(req.headers.date));
-                // console.log(req.headers.date.split("-"));
+                var comingFromClientDate = new Date(req.headers.date);
+
+                for(var i = 0; i < 7; i++)
+                {
+                    var tmp = comingFromClientDate.setTime(comingFromClientDate.getTime() + (j * 24 * 60 * 60 * 1000));
+                    var tmpObj = new Date(tmp);
+                    console.log(tmpObj.getDate().toString().concat("-", (tmpObj.getMonth()+1).toString(), "-", tmpObj.getFullYear()));
+                    j = 1;
+                }
                 result = await searchServiceObject.getListFlights(req.headers.fromplace, req.headers.toplace, req.headers.date);
             }else{
                 result = "Введите правильную дату!";
