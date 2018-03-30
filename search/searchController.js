051cff94b461c420c6f35101735c78750ef9cea8
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2018-03-30 16:00:14 +0300

    t

--- search/searchController.js
+++ search/searchController.js
@@ -3,23 +3,21 @@ var searchServiceObject = new searchService.SearchService();
 
 class SearchController {
 
+
+
         async getListFlights(req, res) {
             var result;
-            console.log(/^\d{4}-\d{1,2}-\d\d$/.test(req.headers.date));
 
-
+            if(/^\d{4}-\d{1,2}-\d\d$/.test(req.headers.date)){
+                console.log(new Date(req.headers.date));
+                // console.log(req.headers.date.split("-"));
+                result = await searchServiceObject.getListFlights(req.headers.fromplace, req.headers.toplace, req.headers.date);
+            }else{
+                result = "Введите правильную дату!";
+            }
 
             try{
-
-                if(/^\d{4}-\d{1,2}-\d\d$/.test(req.headers.date)){
-                    result = await searchServiceObject.getListFlights(req.headers.fromplace, req.headers.toplace, req.headers.date);
-                }else{
-                    result = "Введите правильную дату!";
-                }
-
-                //console.log(result);
                 res.send(result);
-                //res.render('index', { name: result });
             }catch(error) {
                 console.log("Error");
             }
