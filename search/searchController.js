01245eba00d54441a6e08005c22dfd6d2a12a301
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2018-03-31 10:57:39 +0300

    date

--- search/searchController.js
+++ search/searchController.js
@@ -9,14 +9,28 @@ class SearchController {
             if(/^\d{4}-\d{1,2}-\d\d$/.test(req.headers.date)){
                 var comingFromClientDate = new Date(req.headers.date);
 
-                for(var i = 0; i < 7; i++)
-                {
-                    var tmp = comingFromClientDate.setTime(comingFromClientDate.getTime() + (j * 24 * 60 * 60 * 1000));
-                    var tmpObj = new Date(tmp);
-                    console.log(tmpObj.getDate().toString().concat("-", (tmpObj.getMonth()+1).toString(), "-", tmpObj.getFullYear()));
-                    j = 1;
-                }
-                result = await searchServiceObject.getListFlights(req.headers.fromplace, req.headers.toplace, req.headers.date);
+
+                // var tmp = comingFromClientDate.getTime();
+                var requestedByClientDate = new Date((new Date(req.headers.date)).getTime());
+                console.log("Requested date: ", requestedByClientDate);
+                // var requestedDatePlusSeven = new Date(requestedByClientDate.setTime(requestedByClientDate.getTime() + (7 * 24 * 60 * 60 * 1000)));
+                // console.log("RequestedDatePlusSeven", requestedDatePlusSeven);
+                // var requestedDateMinusSeven = new Date(requestedByClientDate.setTime(requestedByClientDate.getTime() - (14 * 24 * 60 * 60 * 1000)));
+                // console.log("RequestedDateMinusSeven", requestedDateMinusSeven);
+
+
+                var tmpObj = requestedByClientDate.getFullYear().toString().concat("-", (requestedByClientDate.getMonth()+1).toString(), "-", requestedByClientDate.getDate());
+
+                console.log(tmpObj);
+
+                // for(var i = 0; i < 7; i++)
+                // {
+                //     var tmp = comingFromClientDate.setTime(comingFromClientDate.getTime() + (j * 24 * 60 * 60 * 1000));
+                //     var tmpObj = new Date(tmp);
+                //     console.log(tmpObj.getDate().toString().concat("-", (tmpObj.getMonth()+1).toString(), "-", tmpObj.getFullYear()));
+                //     j = 1;
+                // }
+                result = await searchServiceObject.getListFlights(req.headers.fromplace, req.headers.toplace, tmpObj);
             }else{
                 result = "Введите правильную дату!";
             }
