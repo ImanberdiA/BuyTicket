54941085ef85f917ec95d443d47073c3c852acf9
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2018-04-12 08:55:46 +0300

    q

--- socreg/routes/auth-routes.js
+++ socreg/routes/auth-routes.js
@@ -17,6 +17,7 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
+
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
